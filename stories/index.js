d1c2b5f13b5be7183478148bd511eba06b7ba806
Bernard Thompson <bethomps@redhat.com>
2019-07-08 20:51:55 +0100

    Addes authentication and related tests

--- stories/index.js
+++ stories/index.js
@@ -8,58 +8,75 @@ import { action } from "@storybook/addon-actions";
 import CommentForm from "../src/components/commentForm";
 import Comment from "../src/components/comment";
 import CommentList from "../src/components/commentList";
+import Header from "../src/components/header/";
+import { MemoryRouter } from "react-router";
+import LoginForm from "../src/components/authentication/loginForm";
 
 
 const post = {
-    id: 1,
-    title: 'Post 1.',
-    link: 'http://www.foo.bar',
-    author: 'Joe Bloggs',
-    comments: [],
-    upvotes: 10
+  id: 1,
+  title: 'Post 1.',
+  link: 'http://www.foo.bar',
+  author: 'Joe Bloggs',
+  comments: [],
+  upvotes: 10
 };
 
 storiesOf("Hacker App/News Form", module).add("default", () => (
-    <NewsForm post={post} handleAdd={action('Post Added')} />));
+  <NewsForm post={post} handleAdd={action('Post Added')} />));
 
 storiesOf("Hacker App/News Item", module)
-    .add("default", () => <NewsItem post={post} upvoteHandler={action("upvoted")} />)
-    .add("No hyperlink", () => <NewsItem post={{ ...post, link: "" }} upvoteHandler={action("upvoted")} />);
+  .add("default", () => <NewsItem post={post} upvoteHandler={action("upvoted")} />)
+  .add("No hyperlink", () => <NewsItem post={{ ...post, link: "" }} upvoteHandler={action("upvoted")} />);
 
 storiesOf('Hacker App/News List', module)
-    .add('default', () => {
-        const defaultPosts = [
-            { ...post, id: 1, title: 'Post 1', upvotes: 10 },
-            { ...post, id: 2, title: 'Post 2', upvotes: 20 },
-            { ...post, id: 3, title: 'Post 3', upvotes: 30 },
-            { ...post, id: 4, title: 'Post 4', upvotes: 40 }
-        ];
-        return <NewsList posts={defaultPosts} />
-    });    
+  .add('default', () => {
+    const defaultPosts = [
+      { ...post, id: 1, title: 'Post 1', upvotes: 10 },
+      { ...post, id: 2, title: 'Post 2', upvotes: 20 },
+      { ...post, id: 3, title: 'Post 3', upvotes: 30 },
+      { ...post, id: 4, title: 'Post 4', upvotes: 40 }
+    ];
+    return <NewsList posts={defaultPosts} />
+  });
 
-    storiesOf("Hacker App/Comment page/Comment Form", module).add("default", () => (
-        <CommentForm post={post} addCommentHandler={action("comment added")} />
-      ));
-      
-      const comment = {
-        id: 1,
-        author: "Joe Bloggs",
-        comment: "I agree with .....",
-        upvotes: 10
-      };
-      
-      storiesOf("Hacker App/Comment page/Comment", module).add("default", () => (
-        <Comment upvoteHandler={action("upvoted")} comment={comment} />
-      ));
-      
-      storiesOf("Hacker App/Comment page/Comment list", module).add("default", () => {
-        const defaultComments = [
-          comment,
-          { ...comment, author: "Jane Smith", upvotes: 3, id: 2 },
-          { ...comment, comment: "On the other hand", id: 3 },
-          { ...comment, author: "Jill Dwyer", upvotes: 5, id: 4 }
-        ];
-        return (
-          <CommentList upvoteHandler={action("upvoted")} comments={defaultComments} />
-        );
-      });    
+storiesOf("Hacker App/Comment page/Comment Form", module).add("default", () => (
+  <CommentForm post={post} addCommentHandler={action("comment added")} />
+));
+
+const comment = {
+  id: 1,
+  author: "Joe Bloggs",
+  comment: "I agree with .....",
+  upvotes: 10
+};
+
+storiesOf("Hacker App/Comment page/Comment", module).add("default", () => (
+  <Comment upvoteHandler={action("upvoted")} comment={comment} />
+));
+
+storiesOf("Hacker App/Comment page/Comment list", module).add("default", () => {
+  const defaultComments = [
+    comment,
+    { ...comment, author: "Jane Smith", upvotes: 3, id: 2 },
+    { ...comment, comment: "On the other hand", id: 3 },
+    { ...comment, author: "Jill Dwyer", upvotes: 5, id: 4 }
+  ];
+  return (
+    <CommentList upvoteHandler={action("upvoted")} comments={defaultComments} />
+  );
+});
+
+storiesOf("Hacker App/Header", module)
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+  ))
+  .add("default", () => <Header />);
+
+  storiesOf("Hacker App/Login Form", module)
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+  ))
+  .add("default", () => (
+    <LoginForm />
+  ));
