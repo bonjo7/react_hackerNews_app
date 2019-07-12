81fe872bfadc8765297fd37c83f8c496cce0bba9
Bernard Thompson <bethomps@redhat.com>
2019-07-12 12:17:24 +0100

    Protected comments view

--- src/index.js
+++ src/index.js
@@ -6,21 +6,22 @@ import App from "./App";
 import CommentPage from "./components/commentPage";
 import Header from "./components/header/";
 import LoginForm from "./components/authentication/loginForm";
+import PrivateRoute from "./components/authentication/privateRoute";
 
-const Router = () => 
-    <BrowserRouter>
-      <div className="jumbotron">
-        <Header />
-        <div className="container-fluid">
-          <Switch>
-            <Route path="/posts/:post_id" component={CommentPage} />
-            <Route path="/login" component={LoginForm} />  {/* New route */}
-            <Route exact path="/" component={App} />
-            <Redirect from="*" to="/" />
-          </Switch>
-        </div>
+const Router = () =>
+  <BrowserRouter>
+    <div className="jumbotron">
+      <Header />
+      <div className="container-fluid">
+        <Switch>
+          <PrivateRoute path="/posts/:post_id" component={CommentPage} />
+          <Route path="/login" component={LoginForm} />  {/* New route */}
+          <Route exact path="/" component={App} />
+          <Redirect from="*" to="/" />
+        </Switch>
       </div>
-    </BrowserRouter>
+    </div>
+  </BrowserRouter>
 
 ReactDOM.render(<Router />, document.getElementById("root"));
 
